136ca2e59ade61cbad9a4bddc66a77db0c85580c
tthanquol <tthanquol@gmail.com>
2014-11-30 18:50:00 +0100

    Finish 0.1 version

--- IziResajzer/Form1.Designer.vb
+++ IziResajzer/Form1.Designer.vb
@@ -22,9 +22,134 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnZastosuj = New System.Windows.Forms.Button()
+        Me.lbxFileList = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnAddFiles = New System.Windows.Forms.Button()
+        Me.btnRemoveFiles = New System.Windows.Forms.Button()
+        Me.cbxSizes = New System.Windows.Forms.ComboBox()
+        Me.PictureBox = New System.Windows.Forms.PictureBox()
+        Me.ProgressBar = New System.Windows.Forms.ProgressBar()
+        Me.lblProgress = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnZastosuj
+        '
+        Me.btnZastosuj.Location = New System.Drawing.Point(197, 227)
+        Me.btnZastosuj.Name = "btnZastosuj"
+        Me.btnZastosuj.Size = New System.Drawing.Size(75, 23)
+        Me.btnZastosuj.TabIndex = 0
+        Me.btnZastosuj.Text = "Zastosuj"
+        Me.btnZastosuj.UseVisualStyleBackColor = True
+        '
+        'lbxFileList
+        '
+        Me.lbxFileList.FormattingEnabled = True
+        Me.lbxFileList.Location = New System.Drawing.Point(13, 27)
+        Me.lbxFileList.Name = "lbxFileList"
+        Me.lbxFileList.Size = New System.Drawing.Size(259, 69)
+        Me.lbxFileList.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
+        'btnAddFiles
+        '
+        Me.btnAddFiles.Location = New System.Drawing.Point(16, 102)
+        Me.btnAddFiles.Name = "btnAddFiles"
+        Me.btnAddFiles.Size = New System.Drawing.Size(22, 23)
+        Me.btnAddFiles.TabIndex = 3
+        Me.btnAddFiles.Text = "+"
+        Me.btnAddFiles.UseVisualStyleBackColor = True
+        '
+        'btnRemoveFiles
+        '
+        Me.btnRemoveFiles.Location = New System.Drawing.Point(41, 102)
+        Me.btnRemoveFiles.Name = "btnRemoveFiles"
+        Me.btnRemoveFiles.Size = New System.Drawing.Size(22, 23)
+        Me.btnRemoveFiles.TabIndex = 4
+        Me.btnRemoveFiles.Text = "-"
+        Me.btnRemoveFiles.UseVisualStyleBackColor = True
+        '
+        'cbxSizes
+        '
+        Me.cbxSizes.FormattingEnabled = True
+        Me.cbxSizes.Items.AddRange(New Object() {"1024x768", "800x600", "640x480"})
+        Me.cbxSizes.Location = New System.Drawing.Point(13, 132)
+        Me.cbxSizes.Name = "cbxSizes"
+        Me.cbxSizes.Size = New System.Drawing.Size(81, 21)
+        Me.cbxSizes.TabIndex = 5
+        '
+        'PictureBox
+        '
+        Me.PictureBox.Location = New System.Drawing.Point(105, 103)
+        Me.PictureBox.Name = "PictureBox"
+        Me.PictureBox.Size = New System.Drawing.Size(167, 103)
+        Me.PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox.TabIndex = 6
+        Me.PictureBox.TabStop = False
+        '
+        'ProgressBar
+        '
+        Me.ProgressBar.Location = New System.Drawing.Point(13, 227)
+        Me.ProgressBar.Name = "ProgressBar"
+        Me.ProgressBar.Size = New System.Drawing.Size(178, 23)
+        Me.ProgressBar.TabIndex = 7
+        '
+        'lblProgress
+        '
+        Me.lblProgress.AutoSize = True
+        Me.lblProgress.Location = New System.Drawing.Point(16, 208)
+        Me.lblProgress.Name = "lblProgress"
+        Me.lblProgress.Size = New System.Drawing.Size(43, 13)
+        Me.lblProgress.TabIndex = 8
+        Me.lblProgress.Text = "Postęp:"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.Filter = "Wszystkie pliki (*.*)|*.*|JPEG (*.jpg)|*.jpg"
+        Me.OpenFileDialog1.FilterIndex = 2
+        Me.OpenFileDialog1.Multiselect = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.lblProgress)
+        Me.Controls.Add(Me.ProgressBar)
+        Me.Controls.Add(Me.PictureBox)
+        Me.Controls.Add(Me.cbxSizes)
+        Me.Controls.Add(Me.btnRemoveFiles)
+        Me.Controls.Add(Me.btnAddFiles)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lbxFileList)
+        Me.Controls.Add(Me.btnZastosuj)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents btnZastosuj As System.Windows.Forms.Button
+    Friend WithEvents lbxFileList As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnAddFiles As System.Windows.Forms.Button
+    Friend WithEvents btnRemoveFiles As System.Windows.Forms.Button
+    Friend WithEvents cbxSizes As System.Windows.Forms.ComboBox
+    Friend WithEvents PictureBox As System.Windows.Forms.PictureBox
+    Friend WithEvents ProgressBar As System.Windows.Forms.ProgressBar
+    Friend WithEvents lblProgress As System.Windows.Forms.Label
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
 
 End Class
--- IziResajzer/Form1.vb
+++ IziResajzer/Form1.vb
@@ -1,3 +1,87 @@
 ﻿Public Class Form1
 
+    Private Sub btnAddFiles_Click(sender As Object, e As EventArgs) Handles btnAddFiles.Click
+
+        Dim strFiles() As String
+        Dim strFile As String
+
+        'dodaj zaznaczone pliki do lstFiles
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            strFiles = OpenFileDialog1.FileNames
+            For Each strFile In strFiles
+                lbxFileList.Items.Add(strFile)
+
+            Next
+
+        End If
+    End Sub
+
+
+    'wyswietl plik zaznaczony w lstFiles przy pomocy PictureBox
+    Private Sub lbxFileList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lbxFileList.SelectedIndexChanged
+        If lbxFileList.SelectedItems.Count > 0 Then 'jesli zaznnaczono minimum jeden plik
+
+            Try
+                PictureBox.Image = Image.FromFile(lbxFileList.SelectedItem)
+            Catch ex As Exception
+                MessageBox.Show("Nie udało się otworzyć pliku", "Błąd oczytu pliku", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+
+    End Sub
+
+    Private Sub btnRemoveFiles_Click(sender As Object, e As EventArgs) Handles btnRemoveFiles.Click
+        Do While (lbxFileList.SelectedItems.Count > 0)
+            lbxFileList.Items.Remove(lbxFileList.SelectedItem)
+        Loop
+    End Sub
+
+    Private Sub btnZastosuj_Click(sender As Object, e As EventArgs) Handles btnZastosuj.Click
+        Dim ResizedImage As Image
+        Dim NewSize As New Size
+        Dim strFile As String
+        Dim strResizedName As String
+        If lbxFileList.Items.Count < 1 Then
+            MessageBox.Show("Nie wybrano plików", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Return
+        End If
+        If cbxSizes.SelectedIndex < 0 Then
+            MessageBox.Show("Nie wybrano rozmiaru", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Return
+        End If
+        If cbxSizes.SelectedIndex = 0 Then
+            NewSize.Width = 1024
+            NewSize.Height = 768
+        End If
+        If cbxSizes.SelectedIndex = 1 Then
+            NewSize.Width = 800
+            NewSize.Height = 600
+        End If
+        If cbxSizes.SelectedIndex = 2 Then
+            NewSize.Width = 640
+            NewSize.Height = 480
+        End If
+        PictureBox.Image = Nothing
+        ProgressBar.Minimum = 0
+        ProgressBar.Maximum = lbxFileList.Items.Count
+        ProgressBar.Value = 0
+        ProgressBar.Visible = True
+        Try
+            For Each strFile In lbxFileList.Items
+                ResizedImage = New Bitmap(Image.FromFile(strFile), NewSize)
+                strResizedName = strFile.Substring(0, strFile.LastIndexOf(".")) + "_resized" + ".jpg"
+                ResizedImage.Save(strResizedName, System.Drawing.Imaging.ImageFormat.Jpeg)
+                ProgressBar.Value = ProgressBar.Value + 1
+                ResizedImage.Dispose()
+            Next
+            lbxFileList.Items.Clear()
+        Catch ex As Exception
+            MessageBox.Show("Zapis jednego lub więcej plików nie powiódł się", "Bład", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        MessageBox.Show("Zapisano pomyślnie wszystkie pliki", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.None)
+        ProgressBar.Visible = False
+        lblProgress.Visible = False
+    End Sub
 End Class
+
